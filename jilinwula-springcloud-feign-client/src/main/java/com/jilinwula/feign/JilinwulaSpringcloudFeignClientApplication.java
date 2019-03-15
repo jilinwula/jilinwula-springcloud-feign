0d1f936f5cd57e48fa7611556a8fa06371aa6252
吉林乌拉 <admin@jilinwula.com>
2019-03-15 14:47:16 +0800

    SrpingCloud服务调用feign

---
+++ jilinwula-springcloud-feign-client/src/main/java/com/jilinwula/feign/JilinwulaSpringcloudFeignClientApplication.java
@@ -0,0 +1,15 @@
+package com.jilinwula.feign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class JilinwulaSpringcloudFeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JilinwulaSpringcloudFeignClientApplication.class, args);
+    }
+
+}
